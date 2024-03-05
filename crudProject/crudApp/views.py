88e8787d9a6c2e4edf391e462d453258e1e9d60b
om <omkumar1892002@gmail.com>
2024-03-05 11:05:12 +0530

    Created model form class from model class and showed it in the template file

--- crudProject/crudApp/views.py
+++ crudProject/crudApp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .forms import studentRegistration
 
 # Create your views here.
 def addandshow(request):
-    return render(request , "crudApp/addandshow.html")
+    if request.method == "POST":
+        fm = studentRegistration(request.POST)
+        print("Yes it worked..")
+    else:
+        fm = studentRegistration()
+    return render(request , "crudApp/addandshow.html" , {"form" : fm})
