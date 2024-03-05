998a7e613ee4040474d821d3cda60f5dcae769de
om <omkumar1892002@gmail.com>
2024-03-05 11:36:52 +0530

    Added the code to insert the data into the database as soon as user hits on the submit button

--- crudProject/crudApp/views.py
+++ crudProject/crudApp/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from .forms import studentRegistration
+from .models import User
 
 # Create your views here.
 def addandshow(request):
     if request.method == "POST":
         fm = studentRegistration(request.POST)
-        print("Yes it worked..")
+
+        if fm.is_valid():
+            name = fm.cleaned_data["name"] 
+            email = fm.cleaned_data["email"] 
+            password = fm.cleaned_data["password"] 
+
+            regis = User(name=name,email=email,password=password)
+            regis.save()
+            fm = studentRegistration()
     else:
         fm = studentRegistration()
     return render(request , "crudApp/addandshow.html" , {"form" : fm})
